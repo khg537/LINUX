9fe6190a95db4e3e9008c6bce7f15840dd90b05c
khg537 <khg537@gmail.com>
2022-05-04 15:12:40 +0900

    Update ls.h

--- ls.h
+++ ls.h
@@ -10,11 +10,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define LIST  1
-#define INODE 2
-#define RECUR  4
-#define ALL   8
+#define LIST	1
+#define INODE	2
+#define RECUR	4
+#define ALL		8
+
+#define MAX_FILE_LIST_NUM 256 
+#define MAX_DIR_LIST_NUM 256 
+
+typedef struct
+{
+	struct stat buff;
+	char fname[256];
+	int  is_dir;
+} FILEDESC;
+
 
 void  long_list_display(const char*, int);
 void  short_list_display(const char*, int);
-void  dir_search(const char*, int);
+void  dir_display(const char*, int);
+int compare_list_name(const void *, const void *);
